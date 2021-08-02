1182b025d48b7839d524c84d804267d2f8f55b95
Ejim Michael <ejimmichael1@gmail.com>
2021-08-02 21:21:06 +0100

    using material ui to style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['hello'])
+  const [messages, setMessages] = useState(['hello', 'hi', 'whats up'])
    
 console.log(input)
 console.log(messages)
 
 const sendMessage = (event) => {
+  event.preventDefault();
   setMessages([...messages, input]);
   setInput('');
 }
@@ -16,11 +18,18 @@ const sendMessage = (event) => {
     <div className="App">
       <h1>Hello clever programming</h1>
 
+      <form>
       <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={sendMessage}>Send Message</button>
+      <Button variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      </form>
      
      {/* messages themselves */}
-     
+     {
+       messages.map(message => (
+        <p>{message}</p>
+       ))
+     }
+
     </div>
   );
 }
