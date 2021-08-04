c490434247769e4a37d57f0640c1f74d51a26e83
Ejim Michael <ejimmichael1@gmail.com>
2021-08-04 03:08:13 +0100

    react FlipMove

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Button,FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
+import db from './firebase';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([
-    {username: 'Michael', text: 'hey guys'}, 
-    {username: 'Ejim', text: 'whats up'}]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    db.collection('messages')
+    .orderBy('timestamp', 'desc')
+    .onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
+    }) 
+  }, [])
 
   useEffect(() => {
     setUsername(prompt("Please enter your name"))
@@ -16,12 +25,18 @@ function App() {
 
 const sendMessage = (event) => {
   event.preventDefault();
-  setMessages([...messages, { username: username, text: input}]);
+
+  db.collection('messages').add({
+    message: input,
+    username: username,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  })
+
   setInput('');
 }
   return (
     <div className="App">
-      <h1>Hello Team codersHub</h1>
+      <h1>Hello Team coders</h1>
       <h2>Welcome {username}</h2>
 
       <form>
@@ -32,12 +47,13 @@ const sendMessage = (event) => {
       </FormControl>
       </form>
      
-     {/* messages themselves */}
-     {
-       messages.map(message => (
-         <Message username={username} message={message} />
-       ))
-     }
+     <FlipMove>
+      {
+        messages.map(({id, message}) => (
+          <Message key={id} username={username} message={message} />
+        ))
+      }
+     </FlipMove>
 
     </div>
   );
